e9c5fb54e2340278cbff159ff032acebe1dd389a
Justino <herrerajustin11@gmail.com>
2024-09-16 18:13:51 -0700

    Major debugging to make the apollo-client imports and run commands work correctly

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
-      }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@apollo/client': path.resolve(__dirname, './node_modules/@apollo/client')
+    }
+  },
+  optimizeDeps: {
+    include: ['@apollo/client']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
     }
   }
 })
